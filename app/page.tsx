ef219a62684f5561cf638b6aa7268ca8133d8ce7
Saurabh Panth <saurabhpanth26@gmail.com>
2024-08-15 15:20:35 +0530

    sample-application

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is a simple landing page deployed to AWS Amplify.</p>
+    </div>
+  );
+}
